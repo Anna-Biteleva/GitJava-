ceb9f6bfd60c5156c739af655b7a0e73dc10ceb9
Anna-Biteleva <biteleva.anna@gmail.com>
2021-09-21 01:40:26 +0300

    Some additions to the homework

--- Lesson_1/src/HomeWorkApp.java
+++ Lesson_1/src/HomeWorkApp.java
@@ -1,13 +1,19 @@
 public class HomeWorkApp {
 
     public static void main(String[] args) {
-        printThreeWords ("Orange");
-        printThreeWords ("Banana");
-        printThreeWords ("Apple");
+        printThreeWords("Orange");
+        printThreeWords("Banana");
+        printThreeWords("Apple");
         checkSumSign();
+        if (checkSum1(-10, 20))
+            System.out.println("Сумма положительная");
+        else System.out.println("Сумма отрицательная");
         printColor();
         compareNumbers();
+        System.out.println(compareNum (2, 10));
+
     }
+
     // 2. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple.
 
      public static void printThreeWords(String fruit) {
@@ -27,6 +33,12 @@ public class HomeWorkApp {
 
         else System.out.println("Сумма отрицательная");
     }
+    //  Второй вариант третьего метода
+
+    public static boolean checkSum1( int a, int b) {
+        return a + b >= 0;
+    }
+
 
     // 4. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением.
 // Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”, если лежит в пределах от 0 (0 исключительно)
@@ -41,6 +53,8 @@ public class HomeWorkApp {
         } else {
             System.out.println("Зеленый");
         }
+
+
     }
     // 5. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями,
     // которыми захотите. Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
@@ -54,5 +68,12 @@ public class HomeWorkApp {
             else System.out.println("a < b");
 
         }
+// другой вариант метода compareNumbers
 
+   public static String compareNum (int a, int b) {
+    if (a < b)
+        return ("a < b");
+    else
+        return ("a >= b");
+    }
 }
